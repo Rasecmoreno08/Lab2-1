722e14edcdb3bab50fefc0478b98e227e59101bd
odenfis <wmoncada@tecsup.edu.pe>
2023-04-03 09:49:28 -0500

    ver0304_5

--- conexion.php
+++ conexion.php
@@ -1,16 +1,10 @@
 <?php
-
 	function conexion(){
-
 	$host = "host=containers-us-west-38.railway.app";
 	$port = "port=8029";
 	$dbname = "dbname=railway";
 	$user = "user=postgres";
 	$password = "password=JIcL9swq3Y5O3TS911yU";
-
-	$db = pg_connect("$host $port $dbname $user $password");
-
-	return $db;
+	$db = pg_connect("$host $port $dbname $user $password");	
 }
 ?>
-
