f063d3c3c1c9af09f96e8d6a67856800616cab33
odenfis <wmoncada@tecsup.edu.pe>
2023-04-03 09:37:34 -0500

    ver0304_3

--- conexion.php
+++ conexion.php
@@ -2,11 +2,11 @@
 
 	function conexion(){
 
-	$host = "host=containers-us-west-38.railway.app";
-	$port = "port=8029";
-	$dbname = "dbname=railway";
-	$user = "user=postgres";
-	$password = "password=JIcL9swq3Y5O3TS911yU";
+	$host = "containers-us-west-38.railway.app";
+	$port = "8029";
+	$dbname = "railway";
+	$user = "postgres";
+	$password = "JIcL9swq3Y5O3TS911yU";
 
 	$db = pg_connect("$host $port $dbname $user $password");
 
